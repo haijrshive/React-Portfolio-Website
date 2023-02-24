4315e0069fe0a1d2f0b8c6b1c79924a769dcd44b
Hai Shive Jr <haishivejr@gmail.com>
2023-02-23 20:23:20 -0800

    contact css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,13 +11,19 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <AiOutlineMail/>
+            <AiOutlineMail className="contact__option-icon"/>
             <h4>Email</h4>
             <h5>haishivejr@gmail.com</h5>
-            <a href="mailto:haishivejr@gmail.com">Send a message</a>
+            <a href="mailto:haishivejr@gmail.com" target="_blank">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required ></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
       </div>
     </section>
   )
