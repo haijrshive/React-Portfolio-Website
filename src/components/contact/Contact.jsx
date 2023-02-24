25e1c93b3228d8dfc108ed967038bd06ba5d0a3c
Hai Shive Jr <haishivejr@gmail.com>
2023-02-23 18:51:00 -0800

    header color change

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+      </div>
+    </section>
   )
 }
 
-export default Contact
+export default Contact;
