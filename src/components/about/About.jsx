d5698c20e6001a77eb22978d81394ed5aead4dea
Hai Shive Jr <haishivejr@gmail.com>
2023-02-27 12:48:28 -0800

    ME image added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './about.css';
-// import ME from "../../assets/me-about.jpg";
+// import ME from "../../assets/me-about.jpeg";
 import {TbMilitaryAward, TbUsers} from "react-icons/tb";
 import {VscFolderOpened} from "react-icons/vsc";
 
@@ -13,7 +13,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            {/* <img src={ME} alt='About Image' /> */}
+            {/* <img src={ME} alt='Me' /> */}
           </div>
         </div>
 
