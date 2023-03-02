30f57da90c7400545e02e8e83e14d8533e4778a4
Hai Shive Jr <haishivejr@gmail.com>
2023-03-01 18:34:22 -0800

    Services omitted

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import "./nav.css";
 import {AiOutlineHome} from "react-icons/ai";
 import {BiUser} from "react-icons/bi";
 import {BiBookOpen} from "react-icons/bi";
-import {MdMiscellaneousServices} from "react-icons/md";
+// import {MdMiscellaneousServices} from "react-icons/md";
 import {BiMessageAltDetail} from "react-icons/bi";
 
 const Nav = () => {
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><BiUser/></a>
       <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBookOpen/></a>
-      <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><MdMiscellaneousServices/></a>
+      {/* <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><MdMiscellaneousServices/></a> */}
       <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><BiMessageAltDetail/></a>
     </nav>
   )
